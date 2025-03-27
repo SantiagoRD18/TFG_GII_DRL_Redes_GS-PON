addedf9d9c03d9e3f4ed1bcdd7d32857692dae2b
Santi <rodriguezdiezsantiago18@gmail.com>
2025-03-27 20:32:29 +0100

    Simulador inicial
    Scripts y ficheros del simulador de redes PON original, de Víctor Herrezuelo Paredes

---
+++ Simulador/main.py
@@ -0,0 +1,22 @@
+### Importamos módulos propios
+from ejecutar_simulacion import ejecutar_simulacion
+from calcular_estadisticas import calcular_estadisticas
+from packages.configuration.configuration import *
+import time
+import os
+import csv
+
+
+## Simulación
+def main():
+    for i in range(len(CONFIG_CARGA)):
+        # Ejecutamos la simulacion
+        try:
+            ejecutar_simulacion(CONFIG_CARGA[i])
+        except Exception as e:
+            # Atrapar cualquier excepción
+            print(f"Ha ocurrido un error: {e}")
+
+
+if __name__ == '__main__':
+    main()
